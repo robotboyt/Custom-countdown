cca0eabc133a71e470a8446ecc7bfef3e0d7df55
robotboyt <bokachodantes@gmail.com>
2022-03-18 16:24:54 +0200

    added localStorage and any func

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ const countdownBtn = document.getElementById('countdown-button')
 const timeElements = document.querySelectorAll('span')
 
 
+const completeEl = document.getElementById('complete')
+const completeElInfo = document.getElementById('complete-info')
+const completeBtn  = document.getElementById("complete-button")
+
+
 let countdownTitle = ''
 let countdownDate = ''
 let countdownValue = Date
+let countdownActive
+let savedCountdown
 
 const second  = 1000 
 const minute = second * 60
@@ -28,25 +35,33 @@ function updateDOM() {
    countdownActive = setInterval(() => {
     const now = new Date().getTime()
     const distance = countdownValue - now 
-    console.log("distance", distance);
  
     const days =  Math.floor(distance / day)
     const hours = Math.floor((distance % day) / hour)
     const minutes = Math.floor((distance % hour) / minute)
     const seconds = Math.floor((distance % minute) / second)
-    console.log(days, hours, minutes , seconds )
+
+    //    Hide inpute 
+    inputContainer.hidden = true
+
+    // if the countdown has ended, show complete
+    if (distance < 0) {
+        countdownEl.hidden = true
+        clearInterval(countdownActive)
+        completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate} `
+        completeEl.hidden = false
+    } else {
+        // Else, show the countdown progress
  
-     // Populate CountDown
-     countdownElTitle.textContent = `${countdownTitle}`
-     timeElements[0].textContent = `${days}`
-     timeElements[1].textContent = `${hours}`
-     timeElements[2].textContent = `${minutes}`
-     timeElements[3].textContent = `${seconds}`
-     
-     //    Hide inpute 
-     inputContainer.hidden = true
-     // Show Countdown
-     countdownEl.hidden = false
+        countdownElTitle.textContent = `${countdownTitle}`
+        timeElements[0].textContent = `${days}`
+        timeElements[1].textContent = `${hours}`
+        timeElements[2].textContent = `${minutes}`
+        timeElements[3].textContent = `${seconds}`
+        completeEl.hidden = true
+        countdownEl.hidden = false
+        
+    }
    }, second)
 }
 
@@ -55,14 +70,18 @@ function updateCountdown(e) {
     e.preventDefault()
     countdownTitle = e.srcElement[0].value
     countdownDate = e.srcElement[1].value
-    console.log(countdownTitle, countdownDate);
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate,
+    }
+    console.log(savedCountdown);
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown))
     //  Check for valid date 
     if(countdownDate === '') {
         alert("Please selec a date for the countdown.")
     } else {
         //  Get numbet version of current Date, updateDom
         countdownValue = new Date(countdownDate).getTime()
-        console.log('countdown value:', countdownValue);
         updateDOM()
     }
 }
@@ -71,14 +90,34 @@ function updateCountdown(e) {
 function reset() {
     // Hide Countdowns, show Input
     countdownEl.hidden = true
+    completeEl.hidden = true
     inputContainer.hidden = false
     // Stop the countdown
     clearInterval(countdownActive)
     // Reset values 
     countdownTitle = ''
     countdownDate = ''
+    localStorage.removeItem('countdown')
+}
+
+
+// Save Date in LocalStorage for refresh the page 
+function restorePreviousCountdown(){
+    // Get countdown from LocalStorage if available
+    if(localStorage.getItem('countdown')) {
+        inputContainer.hidden = true
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'))
+        countdownTitle = savedCountdown.title
+        countdownDate = savedCountdown.date
+        countdownValue = new Date(countdownDate).getTime()
+        updateDOM()
+    }
 }
 
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown)
-countdownBtn.addEventListener('click', reset )
+countdownBtn.addEventListener('click', reset)
+completeBtn.addEventListener('click', reset)
+
+// On Load, check localStorage
+restorePreviousCountdown()
