871c29e40ce9071f7e5e73ac9bf6ec12ac4365c6
robotboyt <bokachodantes@gmail.com>
2022-03-18 11:37:21 +0200

    added functional and added updateCountDown with DOM

---
+++ script.js
@@ -0,0 +1,84 @@
+const inputContainer = document.getElementById('input-container')
+const countdownForm = document.getElementById('countdownForm')
+const dateEl = document.getElementById('date-picker')
+
+const countdownEl = document.getElementById('countdown')
+const countdownElTitle = document.getElementById('countdown-title')
+const countdownBtn = document.getElementById('countdown-button')
+const timeElements = document.querySelectorAll('span')
+
+
+let countdownTitle = ''
+let countdownDate = ''
+let countdownValue = Date
+
+const second  = 1000 
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+
+
+// Set Data Input Min with Today's Date
+const today = new Date().toISOString().split('T')[0]
+dateEl.setAttribute('min', today)
+
+//  Populate Countdown / Complate UI
+
+function updateDOM() {
+   countdownActive = setInterval(() => {
+    const now = new Date().getTime()
+    const distance = countdownValue - now 
+    console.log("distance", distance);
+ 
+    const days =  Math.floor(distance / day)
+    const hours = Math.floor((distance % day) / hour)
+    const minutes = Math.floor((distance % hour) / minute)
+    const seconds = Math.floor((distance % minute) / second)
+    console.log(days, hours, minutes , seconds )
+ 
+     // Populate CountDown
+     countdownElTitle.textContent = `${countdownTitle}`
+     timeElements[0].textContent = `${days}`
+     timeElements[1].textContent = `${hours}`
+     timeElements[2].textContent = `${minutes}`
+     timeElements[3].textContent = `${seconds}`
+     
+     //    Hide inpute 
+     inputContainer.hidden = true
+     // Show Countdown
+     countdownEl.hidden = false
+   }, second)
+}
+
+// take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault()
+    countdownTitle = e.srcElement[0].value
+    countdownDate = e.srcElement[1].value
+    console.log(countdownTitle, countdownDate);
+    //  Check for valid date 
+    if(countdownDate === '') {
+        alert("Please selec a date for the countdown.")
+    } else {
+        //  Get numbet version of current Date, updateDom
+        countdownValue = new Date(countdownDate).getTime()
+        console.log('countdown value:', countdownValue);
+        updateDOM()
+    }
+}
+
+// Reset All Value 
+function reset() {
+    // Hide Countdowns, show Input
+    countdownEl.hidden = true
+    inputContainer.hidden = false
+    // Stop the countdown
+    clearInterval(countdownActive)
+    // Reset values 
+    countdownTitle = ''
+    countdownDate = ''
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown)
+countdownBtn.addEventListener('click', reset )
